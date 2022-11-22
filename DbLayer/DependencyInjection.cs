e49d779eb4f147dbed7c5af28a397e0e52e9dfcf
Adelvig <adelvig@yandex.ru>
2022-11-22 17:03:54 +0700

    Реализация внедрения зависимостей

---
+++ DbLayer/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using DbLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DbLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DcConnection"];
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            //services.AddScoped<IUsersDbContext>(provider => provider.GetService<ApplicationDbContext>());
+            services.AddScoped<IReportDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            return services;
+        }
+    }
+}
